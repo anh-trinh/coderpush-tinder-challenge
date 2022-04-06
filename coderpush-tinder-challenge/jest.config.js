dcabda374bd0149e71d4776b76ee777390f31437
anh-trinh <anh-trinh@outlook.com>
2022-04-06 16:40:55 +0700

    Revert "feat(set-up): add jest-dom, #26"
    This reverts commit adde6d4c

--- coderpush-tinder-challenge/jest.config.js
+++ coderpush-tinder-challenge/jest.config.js
@@ -3,11 +3,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  moduleDirectories: ["node_modules", "src"],
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest"
   }
 };
